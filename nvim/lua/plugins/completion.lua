076483967fd45391a0356282c6f5d6563ced3974
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-12-23 23:18:49 +0100

    feat(nvim): remove snippets from completions

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -21,7 +21,7 @@ return {
         nerd_font_variant = 'mono'
       },
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { 'lsp', 'path', 'buffer' },
 
         -- Disable cmdline completions
         cmdline = {},
