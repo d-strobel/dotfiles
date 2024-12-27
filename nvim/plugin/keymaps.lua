8c6f32dc4786fa712750199d001876cdda1febf5
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-12-27 14:48:44 +0100

    feat(nvim): add keymap for pasting without yanking

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -1,64 +1,68 @@
-local keymap = vim.keymap
+local keymap = vim.keymap.set
 
 -- Remove highlight search
-keymap.set('n', '<ESC>', '<cmd>nohlsearch<cr>')
+keymap('n', '<ESC>', '<cmd>nohlsearch<cr>')
 
 -- Disable space in normal mode
-keymap.set("n", "<Space>", "<Nop>")
+keymap("n", "<Space>", "<Nop>")
 
 -- Move multilines in visual mode
-keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
-keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
+keymap("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+keymap("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 
 -- Centralize while going page up and down
-keymap.set("n", "<C-d>", "<C-d>zz")
-keymap.set("n", "<C-u>", "<C-u>zz")
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
 
 -- Void registry pasting
-keymap.set("x", "<leader>p", [["_dP]])
+keymap("x", "<leader>p", [["_dP]])
 
 -- Copy to system clipboard
-keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-keymap.set("n", "<leader>Y", [["+Y]])
+keymap({ "n", "v" }, "<leader>y", [["+y]])
+keymap("n", "<leader>Y", [["+Y]])
 
 -- Paste form system clipboard
-keymap.set({ "n", "v" }, "<leader>p", [["+p]])
-keymap.set("n", "<leader>P", [["+P]])
+keymap({ "n", "v" }, "<leader>p", [["+p]])
+keymap("n", "<leader>P", [["+P]])
+
+-- Paste over currently selected text without yanking it
+keymap("v", "p", '"_dp')
+keymap("v", "P", '"_dP')
 
 -- Delete to void registry
-keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+keymap({ "n", "v" }, "<leader>d", [["_d]])
 
 -- Sarch and replace in current file
-keymap.set("n", "<leader>sr", ":%s/")
-keymap.set("v", "<leader>sr", "y:%s/<C-r>\"", { noremap = true, silent = false })
+keymap("n", "<leader>sr", ":%s/")
+keymap("v", "<leader>sr", "y:%s/<C-r>\"", { noremap = true, silent = false })
 
 -- Sarch and replace in quickfix
-keymap.set("n", "<leader>qr", ":cdo :%s/")
-keymap.set("n", "<leader>qR", ":cfdo :%s/")
+keymap("n", "<leader>qr", ":cdo :%s/")
+keymap("n", "<leader>qR", ":cfdo :%s/")
 
 -- Open Quickfix list
-keymap.set("n", "<leader>qo", "<cmd>copen<CR>")
-keymap.set("n", "<leader>qc", "<cmd>cclose<CR>")
+keymap("n", "<leader>qo", "<cmd>copen<CR>")
+keymap("n", "<leader>qc", "<cmd>cclose<CR>")
 
 -- Remap cnext and cprev for quickfix lists
-keymap.set('n', '<leader>qj', '<cmd>cnext<CR>zz')
-keymap.set('n', '<leader>qk', '<cmd>cprev<CR>zz')
+keymap('n', '<leader>qj', '<cmd>cnext<CR>zz')
+keymap('n', '<leader>qk', '<cmd>cprev<CR>zz')
 
 -- Diagnostics
-keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+keymap("n", "<leader>d", vim.diagnostic.open_float)
 
 -- Autopairs in insert mode
-keymap.set("i", "(", "()<left>")
-keymap.set("i", "[", "[]<left>")
-keymap.set("i", "{", "{}<left>")
-keymap.set("i", "<", "<><left>")
-keymap.set("i", "\"", "\"\"<left>")
-keymap.set("i", "'", "''<left>")
+keymap("i", "(", "()<left>")
+keymap("i", "[", "[]<left>")
+keymap("i", "{", "{}<left>")
+keymap("i", "<", "<><left>")
+keymap("i", "\"", "\"\"<left>")
+keymap("i", "'", "''<left>")
 
 -- Autopairs in visual mode
-keymap.set("v", "(", "c()<Esc>P<right>")
-keymap.set("v", "[", "c[]<Esc>P<right>")
-keymap.set("v", "{", "c{}<Esc>P<right>")
-keymap.set("v", "<", "c<><Esc>P<right>")
-keymap.set("v", "\"", "c\"\"<Esc>P<right>")
-keymap.set("v", "'", "c''<Esc>P<right>")
+keymap("v", "(", "c()<Esc>P<right>")
+keymap("v", "[", "c[]<Esc>P<right>")
+keymap("v", "{", "c{}<Esc>P<right>")
+keymap("v", "<", "c<><Esc>P<right>")
+keymap("v", "\"", "c\"\"<Esc>P<right>")
+keymap("v", "'", "c''<Esc>P<right>")
