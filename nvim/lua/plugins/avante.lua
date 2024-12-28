bb989775d8df6355df133159c45b3dae6fe65aef
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-12-28 03:00:16 +0100

    feat(nvim): remove avante optional dependencies

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -17,30 +17,6 @@ return {
       -- optional
       "nvim-tree/nvim-web-devicons",
       "zbirenbaum/copilot.lua",
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
     },
   }
 }
