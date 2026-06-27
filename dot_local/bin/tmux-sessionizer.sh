bbb4d584659094b8e00d41830ec152eccd9f55f0
d-strobel <github.6pr5i@slmails.com>
2026-06-27 20:15:37 +0200

    style(tmux): formatting

--- dot_local/bin/tmux-sessionizer.sh
+++ dot_local/bin/tmux-sessionizer.sh
@@ -3,25 +3,25 @@
 SEARCH_PATHS="$HOME/git/github.com/d-strobel $HOME/git/github.com/laser-zentrale-de $HOME/git/gitlab.com/strobel-iac $HOME/git/codeberg.org/d-strobel"
 
 if [[ $# -eq 1 ]]; then
-    selected=$1
+  selected=$1
 else
-    selected=$(fd . --type d --max-depth 1 --full-path $SEARCH_PATHS | fzf --bind=ctrl-l:accept --tmux=bottom --border-label=" Tmux Sessionizer ")
+  selected=$(fd . --type d --max-depth 1 --full-path $SEARCH_PATHS | fzf --bind=ctrl-l:accept --tmux=bottom --border-label=" Tmux Sessionizer ")
 fi
 
 if [[ -z $selected ]]; then
-    exit 0
+  exit 0
 fi
 
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s "$selected_name" -c "$selected"
-    exit 0
+  tmux new-session -s "$selected_name" -c "$selected"
+  exit 0
 fi
 
 if ! tmux has-session -t="$selected_name" 2> /dev/null; then
-    tmux new-session -ds "$selected_name" -c "$selected"
+  tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
 tmux switch-client -t "$selected_name"
--- dot_local/bin/tmux-sessions.sh
+++ dot_local/bin/tmux-sessions.sh
@@ -4,19 +4,19 @@ last_session=$(tmux display-message -p '#{client_last_session}')
 sessions=$(tmux list-sessions -F '#{session_name}' | grep -v "^$last_session$")
 
 if [[ -z $last_session ]]; then
-    selected=$(echo "$sessions" | fzf --bind=ctrl-l:accept --tmux bottom --border-label=" Tmux Sessions ")
+  selected=$(echo "$sessions" | fzf --bind=ctrl-l:accept --tmux bottom --border-label=" Tmux Sessions ")
 else
-    selected=$(echo -e "$last_session\n$sessions" | fzf --bind=ctrl-l:accept --tmux bottom --border-label=" Sessions ")
+  selected=$(echo -e "$last_session\n$sessions" | fzf --bind=ctrl-l:accept --tmux bottom --border-label=" Sessions ")
 fi
 
 if [[ -z $selected ]]; then
-    exit 0
+  exit 0
 fi
 
 selected_name=$(echo "$selected" | sed 's/:.*//')
 
 if ! tmux has-session -t="$selected_name" 2> /dev/null; then
-    tmux new-session -ds "$selected_name" -c "$selected"
+  tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
 tmux switch-client -t "$selected_name"
