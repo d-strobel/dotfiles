425799550ce20c29972cf9181b717eecfbed0d75
d-strobel <github.6pr5i@slmails.com>
2026-06-28 21:10:50 +0200

    refactor(install): back to the roots

--- install.sh
+++ install.sh
@@ -3,24 +3,43 @@
 # Dotfiles install script for debian based devcontainers.
 
 set -e
+
+DOTFILES_SOURCE_PATH="$HOME/dotfiles"
+
+ensure_symlink() {
+    local source="$1"
+    local link="$2"
+
+    # Ensure target directory exists
+    mkdir -p "$(dirname "$link")"
+
+    if [[ "$(readlink "$link" 2>/dev/null)" != "$source" ]]; then
+        # Create or overwrite link
+        echo "Symlink $source -> $link"
+        ln -sfn "$source" "$link"
+    fi
+}
+
 echo "Start devcontainer setup"
 
-# Abort install if fish is not installed
 if ! command -v fish >/dev/null 2>&1; then
-  echo "fish is not installed - abort setup"
-  exit 1
+  echo "Set user shell to fish"
+  sudo chsh vscode --shell "$(which fish)"
+else
+  echo "WARNING - fish is not installed!"
 fi
 
-echo "Set user shell to fish"
-sudo chsh vscode --shell "$(which fish)"
+# Symlink dotfiles
+ensure_symlink "$DOTFILES_SOURCE_PATH/devcontainer/mise" "$HOME/.config/mise"
+ensure_symlink "$DOTFILES_SOURCE_PATH/devcontainer/fish" "$HOME/.config/fish"
+ensure_symlink "$DOTFILES_SOURCE_PATH/dot_config/nvim" "$HOME/.config/nvim"
+ensure_symlink "$DOTFILES_SOURCE_PATH/devcontainer/pi/extensions" "$HOME/.pi/agent/extensions"
+ensure_symlink "$DOTFILES_SOURCE_PATH/devcontainer/pi/keybindings.json" "$HOME/.pi/agent/keybindings.json"
+ensure_symlink "$DOTFILES_SOURCE_PATH/devcontainer/pi/settings.json" "$HOME/.pi/agent/settings.json"
 
-echo "Install mise"
+echo "Deploy mise"
 curl https://mise.run | sh
+"$HOME/.local/bin/mise" install
 
-echo "Symlink mise config"
-ln -s "$HOME/dotfiles/devcontainer/mise" "$HOME/.config/mise"
-
-echo "Mise bootstrap"
-"$HOME/.local/bin/mise" bootstrap
 
 echo "Finished devcontainer setup"
