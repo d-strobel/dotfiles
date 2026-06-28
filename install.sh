0fd251be9acbaab39cec4306b143bf7348d12b2f
d-strobel <github.6pr5i@slmails.com>
2026-06-28 20:34:58 +0200

    refactor(install): dotfile symlinks are now handled with mise bootstrap

--- install.sh
+++ install.sh
@@ -3,43 +3,24 @@
 # Dotfiles install script for debian based devcontainers.
 
 set -e
-
-DOTFILES_SOURCE_PATH="$HOME/dotfiles"
-DOTFILES_TARGET_PATH="$HOME/.config"
-
 echo "Start devcontainer setup"
 
-# Check if fish is installed
+# Abort install if fish is not installed
 if ! command -v fish >/dev/null 2>&1; then
   echo "fish is not installed - abort setup"
   exit 1
 fi
 
-echo "Install mise"
-curl https://mise.run | sh
-
-echo "Remove old dotfiles"
-rm -rf "$DOTFILES_TARGET_PATH/fish"
-rm -rf "$DOTFILES_TARGET_PATH/nvim"
-rm -rf "$DOTFILES_TARGET_PATH/mise"
-rm -rf "$HOME/.pi/agent/extensions"
-
-echo "Create directories"
-mkdir -p "$HOME/.pi/agent"
-
-echo "Link devcontainer dotfiles"
-ln -s "$DOTFILES_SOURCE_PATH/devcontainer/fish" "$DOTFILES_TARGET_PATH/fish"
-ln -s "$DOTFILES_SOURCE_PATH/devcontainer/mise" "$DOTFILES_TARGET_PATH/mise"
-ln -s "$DOTFILES_SOURCE_PATH/dot_config/nvim" "$DOTFILES_TARGET_PATH/nvim"
-ln -s "$DOTFILES_SOURCE_PATH/devcontainer/pi/extensions" "$HOME/.pi/agent/extensions"
-
-echo "Copy pi settings"
-cp --update=none "$DOTFILES_SOURCE_PATH"/devcontainer/pi/*.json "$HOME/.pi/agent/"
-
 echo "Set user shell to fish"
 sudo chsh vscode --shell "$(which fish)"
 
-echo "Install applications via mise"
-"$HOME/.local/bin/mise" install
+echo "Install mise"
+curl https://mise.run | sh
+
+echo "Symlink mise config"
+ln -s "$HOME/dotfiles/devcontainer/mise" "$HOME/.config/mise"
+
+echo "Mise bootstrap"
+"$HOME/.local/bin/mise" bootstrap
 
 echo "Finished devcontainer setup"
